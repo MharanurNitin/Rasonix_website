a75d37837d58a15e7a9e5be37c16d96d2089680f
MharanurNitin <nitinmharanur@gmail.com>
2023-05-09 18:35:40 +0530

    user migration table created

---
+++ database/migrations/2023_05_09_125706_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['guest', 'user', 'editor', 'admin', 'super_admin'])->default('guest');
+            $table->string('profile_image')->nullable()->default(null);
+            $table->timestamp('joining_date')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
