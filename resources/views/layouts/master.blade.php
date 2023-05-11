96e9ab127492e485b8ee7653169ef72e429168f1
MharanurNitin <nitinmharanur@gmail.com>
2023-05-11 22:11:54 +0530

    Rasonix logo added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- tailwind css js file --}}
     <script src="{{url('../assets/js/tailwind.js')}}"></script>
     <title>@yield('title')</title>
 </head>
 
 <body>
+    {{-- navbar file --}}
     @include('layouts.includes.header')
     @yield('content')
+{{-- footer file --}}
     @include('layouts.includes.footer')
 </body>
 
