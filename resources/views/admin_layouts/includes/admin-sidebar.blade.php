487d1a6cae0d8390b74711c64d47f10446cb4d56
Bhavish <bhavish.muneshwar@rasonix.com>
2023-05-15 18:46:17 +0530

    blog option added in admin sidebar

--- resources/views/admin_layouts/includes/admin-sidebar.blade.php
+++ resources/views/admin_layouts/includes/admin-sidebar.blade.php
@@ -38,6 +38,19 @@
                     </div>
 
 
+                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                        <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                        Blog
+                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                    </a>
+                  {{-- Navigation for Add Jobs And View Available Jobs --}}
+                    <div class="nav-link collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="{{url('admin/dashboard/create')}}">Add Blog</a>
+                            <a class="nav-link" href="{{url('admin/dashboard/view-category')}}">View Blog</a>
+                        </nav>
+                    </div>
+
                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                         <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
                         Pages
