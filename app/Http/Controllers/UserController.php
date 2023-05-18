19ea34403385d537b1f1cc25a87a87c70a849f62
MharanurNitin <nitinmharanur@gmail.com>
2023-05-18 13:25:21 +0530

    login error changed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,9 +19,10 @@ class UserController extends Controller
         $user = User::where(['email' => $req->email])->first();
         // $decrypt = Hash::check($user->password);
         if (!$user || !Hash::check($req->password, $user->password)) {
-            return 'username / password not matched';
+            session()->flash('message', 'please enter valid credential');
+            return redirect()->back();
         } else {
-            $req->session()->put(['id'=>$user->id,'name' => $user->name, 'role' => $user->role,'password'=>$user->password]);
+            $req->session()->put(['id' => $user->id, 'name' => $user->name, 'role' => $user->role, 'password' => $user->password]);
             return redirect('admin/dashboard');
         }
     }
@@ -40,21 +41,21 @@ class UserController extends Controller
     {
         // Validate the form data
         $validatedData = $request->validate([
-        'name' => 'required',
-        'email' => 'required|email|unique:users',
-        'phone_no' => 'required|unique:users',
-        'password' => 'required',
-        'role' => 'required|in:guest,user,editor,admin,super_admin',
-        'profile_image' => 'nullable|image',
-        'joining_date' => 'nullable|date',
-        'block_status' => 'boolean',
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone_no' => 'required|unique:users',
+            'password' => 'required',
+            'role' => 'required|in:guest,user,editor,admin,super_admin',
+            'profile_image' => 'nullable|image',
+            'joining_date' => 'nullable|date',
+            'block_status' => 'boolean',
         ]);
-            // Hash the password
+        // Hash the password
         $validatedData['password'] = Hash::make($validatedData['password']);
         // Process the uploaded profile image if provided
         if ($request->hasFile('profile_image')) {
-        $imagePath = $request->file('profile_image')->store('public/profile_images');
-        $validatedData['profile_image'] = $imagePath;
+            $imagePath = $request->file('profile_image')->store('public/profile_images');
+            $validatedData['profile_image'] = $imagePath;
         }
 
         // Create the user
@@ -68,7 +69,7 @@ class UserController extends Controller
     public function view_user()
     {
         $users = User::all();
-        return view('admin_views.admin.view_users',compact('users'));
+        return view('admin_views.admin.view_users', compact('users'));
     }
 
 
@@ -88,8 +89,8 @@ class UserController extends Controller
         // Validate the form data
         $validatedData = $request->validate([
             'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$user->id,
-            'phone_no' => 'required|unique:users,phone_no,'.$user->id,
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'phone_no' => 'required|unique:users,phone_no,' . $user->id,
             'role' => 'required|in:guest,user,editor,admin,super_admin',
             'profile_image' => 'nullable|image',
             'joining_date' => 'nullable|date',
