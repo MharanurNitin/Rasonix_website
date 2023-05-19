44f6d561a6f5cf5196cfbb53d6651d67558aca8d
Bhavish <bhavish.muneshwar@rasonix.com>
2023-05-19 08:59:58 +0530

    category view added

--- resources/views/admin_views/admin/view_category.blade.php
+++ resources/views/admin_views/admin/view_category.blade.php
@@ -1,17 +1,32 @@
 @extends('admin_layouts.master')
 
 @section('content')
-
     <div class="container-fluid px-4">
         <h1 class="mt-4">View Category</h1>
         <ol class="breadcrumb mb-4">
             <li class="breadcrumb-item active">View Category</li>
         </ol>
         <div class="row">
-            @foreach ($data as $item)
-              <h1>{{$item->name}}</h1>
-            @endforeach
-            <div class="col-xl-3 col-md-6">
+            <table class="table  table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Action's</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td><a href="" class="btn btn-primary"><i class="fas fa-solid fa-pen-nib"></i></a>
+                            <a href="" class="btn btn-danger"><i class="fa-regular fa-trash-can"></i></a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {{-- <div class="col-xl-3 col-md-6">
                 <div class="card bg-primary text-white mb-4">
                     <div class="card-body">Primary Card</div>
                     <div class="card-footer d-flex align-items-center justify-content-between">
@@ -19,7 +34,7 @@
                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
